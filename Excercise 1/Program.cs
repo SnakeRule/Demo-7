d8444dd8720cc26d46525b4513675a139a5f7ebe
Jere Valtanen <jerevaltanen@msn.com>
2016-02-02 10:43:36 +0200

    Initial

---
+++ Excercise 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Excercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            string text = null;
+            StreamWriter outputFile = new StreamWriter("SomeText.txt");
+
+            do
+            {
+                Console.Write("Enter some text : ");
+                text = Console.ReadLine();
+                if (text == "")
+                    loop = false;
+                else
+                    outputFile.WriteLine(text);
+
+            } while (loop == true);
+
+            if (outputFile != null)
+            {
+                outputFile.Close();
+            }
+
+
+            try
+            {
+                text = File.ReadAllText("SomeText2.txt");
+
+                Console.WriteLine("You wrote :\n" + text);
+            }
+
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found!");
+            }
+        }
+    }
+}
---
+++ Excercise 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double d;
+            string text;
+            bool integer;
+            bool dubbles;
+
+            System.IO.StreamWriter outputFileInts = new System.IO.StreamWriter("Someints.txt");
+            System.IO.StreamWriter outputFileDoubles = new System.IO.StreamWriter("Somedoubles.txt");
+
+            do
+            {
+                Console.Write("Give a Number (enter or not a number ends) : ");
+                text = Console.ReadLine();
+                integer = (int.TryParse(text, out i)) ;
+                if (integer)
+                {
+                    outputFileInts.WriteLine(i);
+        
+                }
+                dubbles = (Double.TryParse(text, out d));
+                if (dubbles)
+                {
+                    outputFileDoubles.WriteLine(d);
+                }
+
+
+            } while(text.Length != 0);
+
+            outputFileDoubles.Close();
+            outputFileDoubles.Close();
+        }
+    }
+}
